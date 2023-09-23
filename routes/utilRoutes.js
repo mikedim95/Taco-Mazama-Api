f4572d0fa6cd42d123618acb3b5e8e715203fa35
mikedim95 <mikedim95@gmail.com>
2023-09-23 15:28:02 +0300

    testing

--- routes/utilRoutes.js
+++ routes/utilRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 // Define your route
 router.post('/', (req, res, next) => {
-  req.app.locals.globalPublicIP = req.body.publicIP;
-  res.status(200).send(req.app.locals.globalPublicIP);
+  const { publicIP } = req.body;
+  req.app.locals.globalPublicIP = publicIP;
+
   console.log(`Updated the PublicIP from LocalNode to: `);
   console.log(req.app.locals.globalPublicIP);
+
+  res.status(200).json({ publicIP });
 });
 router.get('/', (req, res, next) => {
   res
